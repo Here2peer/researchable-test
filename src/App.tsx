32cae1fa6140e9d309b6032f098dc7b8ec61f951
Here2peer <timothyravor@live.nl>
2022-04-02 13:17:34 +0200

    Added table

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,53 @@ import React from 'react';
 import './App.css';
 
 
-class HammingInput extends React.Component {
+class HammingInput extends React.Component<{}, {hammingArray: Array<any>}> {
     value: string;
-    hammingArray: Array<string>;
+    stringArray: Array<string>;
     hidden = true;
+    index = 0;
 
-    constructor() {
-        super(0);
-        this.handleChange = this.handleChange.bind(this);
+    data = [
+        {
+            label: 'Visualisation',
+            data: [{
+
+            }]
+        }
+    ]
+
+    constructor(props: any) {
+        super(props);
+        this.keyDown = this.keyDown.bind(this);
         this.onChange = this.onChange.bind(this);
         this.value = "";
-        this.hammingArray = [];
-
+        this.stringArray = [];
+        this.state = {
+            hammingArray: []
+        }
     }
 
-    handleChange(e: React.KeyboardEvent<HTMLInputElement>) {
+    keyDown(e: React.KeyboardEvent<HTMLInputElement>) {
         if (e.keyCode === 13) {
-            console.log(this.hidden);
             if (this.value.length === 5) {
-                this.hammingArray.push(this.value);
+                this.stringArray.push(this.value);
                 this.hidden = true;
+                this.updateTable()
             } else {
                 this.hidden = false;
             }
-
-
-            if (this.hammingArray.length > 1) {
-                console.log(this.calcHamming(this.hammingArray[this.hammingArray.length -2], this.hammingArray[this.hammingArray.length-1]))
-            }
         }
     }
+
+    updateTable() {
+        if (this.stringArray.length > 1) {
+            let test = this.calcHamming(this.stringArray[this.stringArray.length -2], this.stringArray[this.stringArray.length-1])
+            this.setState({
+                hammingArray: this.state.hammingArray.concat([test])
+            })
+        }
+    }
+
     onChange(e: React.ChangeEvent<HTMLInputElement>) {
         this.value = e.target.value;
     }
@@ -43,14 +60,14 @@ class HammingInput extends React.Component {
                 distance += 1;
             }
         }
-        return distance
+        return [a, b, distance]
     }
 
     render() {
         return (
             <fieldset>
                 <legend>Enter 5 digits:</legend>
-                <input type="number" onKeyDown={this.handleChange} onChange={this.onChange} />
+                <input type="number" onKeyDown={this.keyDown} onChange={this.onChange} />
                 <p style={{visibility: this.hidden ? 'hidden' : 'visible'}}>
                     Only 5 numbers, you dingus!
                 </p>
@@ -58,6 +75,23 @@ class HammingInput extends React.Component {
                 <p>
                     Press enter to add digits.
                 </p>
+                <br/>
+                <table>
+                    <tr>
+                        <td>String A</td>
+                        <td>String B</td>
+                        <td>Hemming distance</td>
+                    </tr>
+                        {
+                            this.state.hammingArray.map(entry => (
+                            <tr>
+                                <td>{entry[0]}</td>
+                                <td>{entry[1]}</td>
+                                <td>{entry[2]}</td>
+                            </tr>
+
+                            ))}
+                </table>
             </fieldset>
 
         );
